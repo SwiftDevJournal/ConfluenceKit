a0cbff20346528cf208e58dfc1d9855adf6f33cc
Swift Dev Journal <mark@swiftdevjournal.com>
2024-11-12 13:56:05 -0500

    Add the CreateAPI dependencies that ConfluenceKit requires.

--- Package.swift
+++ Package.swift
@@ -5,24 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "ConfluenceKit",
+    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ConfluenceKit",
-            targets: ["ConfluenceKit"]),
+        .library(name: "ConfluenceKit", targets: ["ConfluenceKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kean/Get", from: "2.1.0"),
+        .package(url: "https://github.com/CreateAPI/HTTPHeaders", from: "0.1.0"),
+        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ConfluenceKit",
-            dependencies: []),
-        .testTarget(
-            name: "ConfluenceKitTests",
-            dependencies: ["ConfluenceKit"]),
+        .target(name: "ConfluenceKit", dependencies: [
+            .product(name: "Get", package: "Get"),
+            .product(name: "HTTPHeaders", package: "HTTPHeaders"),
+            .product(name: "URLQueryEncoder", package: "URLQueryEncoder")
+        ], path: "Sources")
     ]
 )
